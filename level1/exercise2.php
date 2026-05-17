48ad6fe73e27a8fb6a98694ca54eb79cd8041247
AlexMartG <amartinezg2@hotmail.com>
2026-05-17 20:59:55 +0200

    Level1 exercise2

---
+++ level1/exercise2.php
@@ -0,0 +1,14 @@
+<?php
+
+$numbers = [10, 20, 30, 40, 50, 60];
+
+echo "Initial size: " . count($numbers) . "<br>";
+
+unset($numbers[3]);
+
+$numbers = array_values($numbers);
+
+echo "Final size: " . count($numbers) . "<br>";
+
+
+?>
