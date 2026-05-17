4f51c45acb53c6098fb3ece75da8dfdc2e0a4388
AlexMartG <amartinezg2@hotmail.com>
2026-05-17 21:48:15 +0200

    Level1 exercise4 completed

---
+++ level1/exercici4.php
@@ -0,0 +1,15 @@
+<?php
+
+$me = [
+    "name" => "Alex",
+    "age" => 38,
+    "email" => "amartinezg2@gmail.com",
+    "favorite_food" => "pasta"
+];
+
+echo "Name: " . $me["name"] . "<br>";
+echo "Age: " . $me["age"] . "<br>";
+echo "Email: " . $me["email"] . "<br>";
+echo "Favorite food: " . $me["favorite_food"];
+
+?>
