2abb56916aa8109775c24396af6e0fe5640618db
AlexMartG <amartinezg2@hotmail.com>
2026-05-17 20:50:06 +0200

    Level1 exercise1 completed

---
+++ level1/exercise1.php
@@ -0,0 +1,15 @@
+<?php
+
+$numbers = [];
+
+$numbers[] = 8;
+$numbers[] = 78;
+$numbers[] = 34;
+$numbers[] = 1;
+$numbers[] = 25;
+
+foreach ($numbers as $number) {
+    echo $number . "<br>";
+}
+
+?>
