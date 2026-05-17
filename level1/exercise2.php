921ec20b5253d7ab6c954bd02a37dc1ed0a421e8
AlexMartG <amartinezg2@hotmail.com>
2026-05-17 21:02:51 +0200

    Level1 exercise2 completed

--- level1/exercise2.php
+++ level1/exercise2.php
@@ -4,11 +4,15 @@ $numbers = [10, 20, 30, 40, 50, 60];
 
 echo "Initial size: " . count($numbers) . "<br>";
 
-unset($numbers[3]);
+unset($numbers[4]);
 
 $numbers = array_values($numbers);
 
 echo "Final size: " . count($numbers) . "<br>";
 
+echo "Final array:<br>";
+foreach ($numbers as $number) {
+    echo $number . "<br>";
+}
 
 ?>
